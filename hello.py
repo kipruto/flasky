d8474bf3e03340522ec3336d87a749da05622d9e
barxwells <barxwells@gmail.com>
2018-10-01 05:40:25 +0300

    second commit

--- hello.py
+++ hello.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def index():
     return '<h1>Hello World</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
